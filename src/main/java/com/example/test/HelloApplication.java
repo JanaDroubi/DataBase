c10c10aa527d74415e9cb472775022bbd07c55a3
JanaDroubi <jana.r.a.droubi@gmail.com>
2024-08-20 12:12:02 +0300

    initial

--- src/main/java/com/example/test/HelloApplication.java
+++ src/main/java/com/example/test/HelloApplication.java
@@ -4,20 +4,52 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+     //  FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+      // FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/example/test/hello-view.fxml"));
+    //   Scene scene = new Scene(fxmlLoader.load(), 900, 600);
+
+
+       FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
+
+
+      // stage.setTitle("Pal_estinian AirLine !");
+     //   stage.setResizable(false); // This line makes the window unsizable
+     //  stage.setScene(scene);
+     //   stage.show();
+
+
+/*
+        try {
+             FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+            Scene scene = new Scene(fxmlLoader.load());
+            stage.setTitle("Hello!");
+            stage.setScene(scene);
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        */
+
+
+
+
+
+
+
     }
 
     public static void main(String[] args) {
         launch();
     }
 }
+
+
+
--- src/main/java/com/example/test/HelloController.java
+++ src/main/java/com/example/test/HelloController.java
@@ -1,39 +1,41 @@
 package com.example.test;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javax.swing.*;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
-public class HelloController {
 
+public class HelloController {
     @FXML
-    private TextField ID1_text;
+   private TextField ID_text;
     @FXML
     private TextField Name_text;
+  // @FXML
+   //private Button Passenger_Add_btn;
 
-    @FXML
-    protected void onClick() {
 
+    //@FXML
+   /* protected void onClick( ) {
+   //if  (this.g== Passenger_Add_btn ){
         Connection con = null;
         PreparedStatement pstmt = null;
 
         try {
             DriverManager.registerDriver(new org.postgresql.Driver());
-            String conInfo = "jdbc:postgresql://localhost:5432/store";
+            String conInfo = "jdbc:postgresql://localhost:5432/Airport_DataBase";
             con = DriverManager.getConnection(conInfo, "postgres", "12345678");
-
-            String qry = "INSERT INTO region (\"New\") VALUES (?)";
+            String qry = "INSERT INTO person (\"p_id\",\"nationality\") VALUES (?, ?)";
             pstmt = con.prepareStatement(qry);
-            pstmt.setInt(1, Integer.parseInt(ID1_text.getText()));
-          //  pstmt.setString(2, Name_text.getText());
-
+            pstmt.setInt(1, Integer.parseInt(ID_text.getText())); // Setting the first placeholder
+            pstmt.setString(2, Name_text.getText());              // Setting the second placeholder
             int rowsAffected = pstmt.executeUpdate();
-
             if (rowsAffected > 0) {
                 JOptionPane.showMessageDialog(null, "Data inserted successfully!");
             } else {
@@ -53,4 +55,90 @@ public class HelloController {
             }
         }
     }
+*/
+/*
+
+    @FXML
+    void add_Button (ActionEvent event) throws IOException {
+
+        //if  (this.g== Passenger_Add_btn ){
+        Connection con = null;
+        PreparedStatement pstmt = null;
+
+        try {
+            DriverManager.registerDriver(new org.postgresql.Driver());
+            String conInfo = "jdbc:postgresql://localhost:5432/Airport_DataBase";
+            con = DriverManager.getConnection(conInfo, "postgres", "12345678");
+            String qry = "INSERT INTO person (\"p_id\",\"nationality\") VALUES (?, ?)";
+            pstmt = con.prepareStatement(qry);
+            pstmt.setInt(1, Integer.parseInt(ID_text.getText())); // Setting the first placeholder
+            pstmt.setString(2, Name_text.getText());              // Setting the second placeholder
+            int rowsAffected = pstmt.executeUpdate();
+            if (rowsAffected > 0) {
+                JOptionPane.showMessageDialog(null, "Data inserted successfully!");
+            } else {
+                JOptionPane.showMessageDialog(null, "Insertion failed.");
+            }
+
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "SQL Error: " + e.getMessage());
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Error: " + e.getMessage());
+        } finally {
+            try {
+                if (pstmt != null) pstmt.close();
+                if (con != null) con.close();
+            } catch (SQLException e) {
+                JOptionPane.showMessageDialog(null, "Error closing resources: " + e.getMessage());
+            }
+        }
+
+
+
+    }
+*/
+
+
+
+
+
+
+
+
+    @FXML
+    protected void onClick1( ) {
+        //if  (this.g== Passenger_Add_btn ){
+        Connection con = null;
+        PreparedStatement pstmt = null;
+
+        try {
+            DriverManager.registerDriver(new org.postgresql.Driver());
+            String conInfo = "jdbc:postgresql://localhost:5432/Airport_DataBase";
+            con = DriverManager.getConnection(conInfo, "postgres", "12345678");
+            String qry = "INSERT INTO person (\"p_id\",\"nationality\") VALUES (?, ?)";
+            pstmt = con.prepareStatement(qry);
+            pstmt.setInt(1, Integer.parseInt(ID_text.getText())); // Setting the first placeholder
+            pstmt.setString(2, Name_text.getText());              // Setting the second placeholder
+            int rowsAffected = pstmt.executeUpdate();
+            if (rowsAffected > 0) {
+                JOptionPane.showMessageDialog(null, "Data inserted successfully!");
+            } else {
+                JOptionPane.showMessageDialog(null, "Insertion failed.");
+            }
+
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "SQL Error: " + e.getMessage());
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Error: " + e.getMessage());
+        } finally {
+            try {
+                if (pstmt != null) pstmt.close();
+                if (con != null) con.close();
+            } catch (SQLException e) {
+                JOptionPane.showMessageDialog(null, "Error closing resources: " + e.getMessage());
+            }
+        }
+    }
+     //   }
+
 }
